0f94bea7b542cfa4905f6ee026da7ec3ceedf2e9
Kostya2702 <loshkarev.kostya27@gmail.com>
2024-11-27 19:55:20 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,74 @@
+from flask import Flask, render_template
+import asyncio
+import datetime
+from netschoolapi import NetSchoolAPI
+import collections
+
+
+app = Flask(__name__)
+
+class Lesson:
+    def __init__(
+            self,
+            day: datetime.datetime,
+            start: datetime.date,
+            end: datetime.date,
+            room: str,
+            number: int,
+            subject: str
+        ):
+            self.day = day
+            self.start = start
+            self.end = end
+            self.room = room
+            self.number = number
+            self.subject = subject
+
+day_names = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"]
+
+async def fetch_lessons():
+    # Проверяем, есть ли сохраненная сессия
+    ns = NetSchoolAPI('https://sgo.edu-74.ru/')
+
+    await ns.login(
+        'ЛошкаревЛ',
+        'cetavale79',
+        'МОУ "СОШ №13"',
+    )
+
+    diary = await ns.diary()
+    lessons_by_weekday = {}
+
+    for day in diary.schedule:
+        for less in day.lessons:
+            weekday = less.day.weekday()
+
+            if weekday not in lessons_by_weekday:
+                lessons_by_weekday[weekday] = []
+
+            lessons_by_weekday[weekday].append(Lesson(
+                less.day,
+                less.start,
+                less.end,
+                less.room,
+                less.number,
+                less.subject
+            ).__dict__)
+
+    print(lessons_by_weekday)
+    # asdf = dict(sorted(lessons_by_weekday.items(), key=lambda item: item[1]))
+    # print(asdf)
+
+    return collections.OrderedDict(
+        (day_names[weekday], lessons) for weekday, lessons in sorted(lessons_by_weekday.items())
+    )
+
+
+@app.route("/")
+def home():
+    lessons_by_weekday = asyncio.run(fetch_lessons())
+    return render_template("diary.html", lessons_by_weekday=lessons_by_weekday)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
